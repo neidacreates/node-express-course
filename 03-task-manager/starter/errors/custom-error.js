13064a4cf0b6d511b91eb5956891f09db2eaf658
Neida <neida@duck.com>
2025-01-10 20:00:32 -0500

    week 6

---
+++ 03-task-manager/starter/errors/custom-error.js
@@ -0,0 +1,12 @@
+class CustomAPIError extends Error{
+  constructor(message, statusCode) {
+    super(message) //invokes constructor of parent class
+    this.statusCode = statusCode
+  }
+}
+
+const createCustomError = (msg, statusCode) => {
+  return new CustomAPIError(msg, statusCode)
+}
+
+module.exports = {createCustomError, CustomAPIError}
